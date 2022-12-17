f98b9c54e857f271a5c8837d237ef08ea44ac82b
alex sanchez <alexjsanchez1020@gmail.com>
2022-12-17 01:03:47 -0500

    add length validation to name

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -4,12 +4,16 @@ exports.checkAccountPayload = (req, res, next) => {
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
    const {name , budget} = req.body;
+   name.trim();
     const error = { status: 400 }
    if(name === undefined || budget === undefined){
     error.message = "name and budget are required"
     next(error)
    }else if(typeof name !== 'string'){
     error.message = "name of account must be a string"
+    next(error)
+   }else if(name.length < 3){
+    error.message= "too short"
     next(error)
    }else{
     res.accPayload = req.body;
